a0a4bfe5348f3e0aa749a918bf4bac3589b64e3e
Jeremy Lee <z5316983@ad.unsw.edu.au>
2023-04-28 02:32:37 +1000

    added sticky navbar

--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -12,28 +12,30 @@ const Navbar = () => {
   const navigate = useNavigate()
 
   return (
-    <nav className='bg-black p-5 flex items-center justify-between drop-shadow-md'>
-      <a className='cursor-pointer scale-75 hover:scale-90 ease-in duration-200' onClick={ () => navigate('/') }>
-        <img src={logo} alt="logo" className="invert h-10"/>  
-      </a>
-      <ul className='flex items-center justify-center gap-2 text-white text-lg'>
-        <li><a className='navLinks' href="#about" >About</a></li>
-        <li><a className='navLinks' href="#projects" >Projects</a></li>
-        <li><a className='navLinks' href="#bookshelf" >Bookshelf</a></li>
-        <li><a className='navLinks' href="#photos" >Photos</a></li>
-        <li><a className='navLinks' href="#contact" >Contact</a></li>
-        <li className='cursor-pointer hover:scale-110 ease-in duration-200 mb-1'
-          onClick={() => {
-            toggleDarkMode(getters, setters)
-          }}
-        >
-          { getters.darkMode === 'true'
-            ? <LightModeIcon></LightModeIcon>
-            : <DarkModeIcon></DarkModeIcon>
-          }
-        </li>
-      </ul>
-    </nav>
+    <div className='sticky top-0 z-50'>
+      <nav className='bg-black p-5 flex items-center justify-between drop-shadow-md'>
+        <a className='cursor-pointer scale-75 hover:scale-90 ease-in duration-200' onClick={ () => navigate('/') }>
+          <img src={logo} alt="logo" className="invert h-10"/>  
+        </a>
+        <ul className='flex items-center justify-center gap-2 text-white text-lg'>
+          <li><a className='navLinks' href="#about" >About</a></li>
+          <li><a className='navLinks' href="#projects" >Projects</a></li>
+          <li><a className='navLinks' href="#bookshelf" >Bookshelf</a></li>
+          <li><a className='navLinks' href="#photos" >Photos</a></li>
+          <li><a className='navLinks' href="#contact" >Contact</a></li>
+          <li className='cursor-pointer hover:scale-110 ease-in duration-200 mb-1'
+            onClick={() => {
+              toggleDarkMode(getters, setters)
+            }}
+          >
+            { getters.darkMode === 'true'
+              ? <LightModeIcon></LightModeIcon>
+              : <DarkModeIcon></DarkModeIcon>
+            }
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
